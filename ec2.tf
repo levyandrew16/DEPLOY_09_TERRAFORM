4eeda4f61159290ca5f135c026cdd1039dba9a76
Levy Andrew <landrew@apps.salcc.edu.lc>
2021-12-04 17:31:17 -0400

    update

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "EC201" {
      instance_type = "t2.micro"
      key_name = "EC2 Tutorial LA"
      subnet_id = "subnet-0341bf4deceb6e1b5"
-     security_groups = "public_sg"
+     #security_groups = "public_sg"
      }
 
 resource "aws_security_group" "public_sg" {
