ed1e6982c5fba4ef1583b462bb2fd7bb63d0a46b
Levy Andrew <landrew@apps.salcc.edu.lc>
2021-12-04 16:03:05 -0400

    Updated Files

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   default_tags {
     tags = {
       Deployment = "DEPLOYMENT_09_TERRAFORM"
--- vpc.tf
+++ vpc.tf
@@ -8,10 +8,42 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "ig1" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "public01" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "Public01"
+  }
+}
+
+resource "aws_subnet" "public02" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "ig1"
+    "Name" = "Public02"
+  }
+}
+
+resource "aws_internet_gateway" "ig1" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name" = "InternetGateway1"
+  }
+}
+
+resource "aws_route_table" "project1-route-table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig1.id
+  }
+
+  tags = {
+    "Name" = "project1-route-table"
   }
 }
